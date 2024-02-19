b5c3aa4d9f9f20c9de15033f45e0a5ff427fadbb
Pandey Bikash <bkppandey87@gmail.com>
2024-02-19 10:16:25 +0545

    Separate game class from play

--- classes/board.rb
+++ classes/board.rb
@@ -2,20 +2,20 @@
 
 # Chess Board
 class Board
-  @@board_count = 0
-  attr_accessor :rows, :columns, :initial_knight_position, :cell
+  attr_accessor :rows, :columns, :initial_knight_position, :cell, :board_count
 
   def initialize
     @rows = 8
     @columns = 8
     @initial_knight_position = [0][1]
     @cell = Array.new(8) { Array.new(2) }
+    @board_count = 0
   end
 
   def populated_cells(column, row)
     if column == 3 && row == 3
       cell[row][column] = '🐴'
-    elsif @@board_count.even?
+    elsif board_count.even?
       cell[row][column] = '⬜'
     else
       cell[row][column] = '⬛'
@@ -24,9 +24,9 @@ class Board
 
   def reverse_colors(row_number)
     if row_number.even?
-      @@board_count = 1
+      @board_count = 1
     else
-      @@board_count = 0
+      @board_count = 0
     end
   end
 
@@ -34,11 +34,10 @@ class Board
     rows.times do |r|
       columns.times do |j|
         print populated_cells(j, r)
-        @@board_count += 1
+        @board_count += 1
       end
       reverse_colors(r)
       puts
     end
   end
 end
-
---
+++ classes/game.rb
@@ -0,0 +1,67 @@
+# Frozen_string_literal: true
+
+require_relative 'color'
+require_relative 'board'
+
+# A Chess game class
+class Game
+  attr_accessor :board, :possible_steps
+
+  def initialize
+    @board = Board.new
+    instructions
+    @possible_steps = []
+  end
+
+  def instructions
+    puts 'SHORTEST PATH FOR THE KNIGHT'.green
+    puts "----------------------------\n\n".blue
+    board.show
+  end
+
+  def check_for_out_of_boundary(new_x, new_y)
+    possible_steps << [new_x, new_y] if new_x > -1 && new_x < 8 && new_y > -1 && new_y < 8
+    possible_steps
+  end
+
+  def calculate_possible_moves(x, y)
+    new_x = x - 1
+    new_y = y - 2
+    check_for_out_of_boundary(new_x, new_y)
+
+    new_x = x + 1
+    new_y = y - 2
+    check_for_out_of_boundary(new_x, new_y)
+
+    new_x = x - 1
+    new_y = y + 2
+    check_for_out_of_boundary(new_x, new_y)
+
+    new_x = x + 1
+    new_y = y + 2
+    check_for_out_of_boundary(new_x, new_y)
+
+    new_x = x - 2
+    new_y = y - 1
+    check_for_out_of_boundary(new_x, new_y)
+
+    new_x = x - 2
+    new_y = y + 1
+    check_for_out_of_boundary(new_x, new_y)
+
+    new_x = x + 2
+    new_y = y + 1
+    check_for_out_of_boundary(new_x, new_y)
+
+    new_x = x + 2
+    new_y = y - 1
+    check_for_out_of_boundary(new_x, new_y)
+  end
+
+  def possible_moves
+    current_knight_position = [2, 2]
+    base_x = 3
+    base_y = 3
+    calculate_possible_moves(base_x, base_y)
+  end
+end
