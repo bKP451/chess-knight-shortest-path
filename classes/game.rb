94f14899f4d7e4bb6b008f2f6fd6353156861193
Pandey Bikash <bkppandey87@gmail.com>
2024-02-20 14:11:25 +0545

    Find a path for a knight to go from one square to another

--- classes/game.rb
+++ classes/game.rb
@@ -3,6 +3,7 @@
 require_relative 'color'
 require_relative 'board'
 require_relative '../modules/possible_moves_helper'
+require_relative 'knight'
 
 # A Chess game class
 class Game
@@ -21,12 +22,30 @@ class Game
     chess_board.show_board
   end
 
-  def bfs(position_of_knight)
-    puts "Current Position of knight is #{position_of_knight}"
-    puts "possible moves are #{knight_possible_moves}"
+  def bfs(start_coords, end_coords)
+    queue = [Knight.new(start_coords)]
+    until queue.empty?
+      knight = queue.shift
+      return knight if knight.position == end_coords
+
+      knight.calculate_possible_moves.each do |move|
+        queue.push(Knight.new(move, knight))
+      end
+    end
   end
 
-  def knight_moves
-    bfs(position_of_knight)
+  def show_traversal_path(start_coords, end_coords)
+    knight = bfs(start_coords, end_coords)
+    path = []
+
+    until knight.nil?
+      path << knight.position
+      knight = knight.parent
+    end
+
+    puts "The traversal path is #{path.size} moves"
+    path.reverse.each do |step|
+      p step
+    end
   end
 end
--- play.rb
+++ play.rb
@@ -3,4 +3,7 @@
 require_relative 'classes/game'
 
 knight_me = Game.new
-knight_me.knight_moves
+initial_x = knight_me.chess_board.initial_knight_position_x
+initial_y = knight_me.chess_board.initial_knight_position_y
+input_square = [initial_x, initial_y]
+knight_me.show_traversal_path(input_square, [6, 6])
